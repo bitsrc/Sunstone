99034e9260a8327e33b0404f0a0deabad7064091
Jason Moss <jason@Krypton.bitsrc.local>
2017-11-17 23:10:51 -0800

    Fixed some linting errors

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import os
-import socket
-import sys
+"""Sunstone
+
+Begins the DHCP server process
+"""
 import SocketServer
 
 from DHCPHandler import DHCPHandler
 
 
 def main():
+    """Begins a socketserver listening on port 67"""
 #    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 #    s.bind(('', 67))
 #    m = s.recvfrom(1024)
 #    print(m)
-    HOST, PORT = "", 67
-    server = SocketServer.UDPServer((HOST, PORT), DHCPHandler)
+    host, port = "", 67
+    server = SocketServer.UDPServer((host, port), DHCPHandler)
     server.serve_forever()
 
 if __name__ == "__main__":
